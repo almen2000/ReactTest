d67216df9a152452ddcf8662cd3677b2c438059e
Alexandr Martirosyan <alexandrmenua@gmail.com>
2019-07-26 04:19:26 +0400

    new changes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyBY6YAitZeDnhv1TYoJx7NDbfMVT93pNTY';
+
 // Create w new component.
 // This component should produce some HTML
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 
